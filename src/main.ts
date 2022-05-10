dce559a3c85c82858700c854fb512a16fdf0d7a6
Quan-Zhe <a0906397408@gmail,com>
2022-05-10 22:17:42 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   await app.listen(3000);
-  console.log('nihau');
+  console.log('hello');
 }
 bootstrap();
 // https://quanzhe.golfourit.com
